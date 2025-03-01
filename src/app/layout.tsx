36f9a0c1537b988f37ed4e77470ba80dac857c2f
anujpanwar01 <anujpanwar7911@gmail.com>
2025-03-01 22:43:11 +0530

    remove the name route segment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,31 +28,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header>
-          <h3>header hai</h3>
-          <nav>
-            <ul>
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-              <li>
-                <Link href="/experience">Experience</Link>
-              </li>
-              <li>
-                <Link href="/education">Portfolio</Link>
-              </li>
-              <li>
-                <Link href="/skills">Skills</Link>
-              </li>
-              <li>
-                <Link href="/projects">Projects</Link>
-              </li>
-              <li>
-                <Link href="/contact">Contact</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
         {children}
       </body>
     </html>
